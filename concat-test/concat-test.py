097f5cee79be3493c9dd73cc9004a007463e2145
Hatem Nassrat <hnassrat@gmail.com>
2013-06-16 16:53:43 +0000

    Flushing stdout after every result (for better use with tee)

--- concat-test/concat-test.py
+++ concat-test/concat-test.py
@@ -8,10 +8,11 @@ MICRO = 1000000 # how many microseconds in a sec
 
 def main():
     test_runs = 1000000
+    test_range = 1000
     w = csv.writer(sys.stdout)
     styles = ['plus', 'join', 'format']
     w.writerow([''] + styles)
-    for i in range(10):
+    for i in range(test_range):
         sr = {}
         for style in styles:
             tr = timeit.timeit(
@@ -21,6 +22,7 @@ def main():
             )
             sr[style] = tr
         w.writerow([str(i+1)] + ['%.3f' % (sr[s] * MICRO / test_runs) for s in styles])
+        sys.stdout.flush()
 
 if __name__ == '__main__':
     main()
